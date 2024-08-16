4fb18c44e8ab53c34ea41eff5a85e132d190d159
ri-gusev <gusev.roman1@mail.ru>
2024-08-16 17:43:14 +0300

    Learned how to use TreeSet and what it is

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        CarCollection array = new CarArrayList();
+        Set<Integer> nums = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+
+        for (int i = 0; i <= 50; i++) {
+            nums.add(i);
+        }
+
+        for (Integer num : nums) {
+            System.out.println(num);
+        }
+
+
     }
 }
