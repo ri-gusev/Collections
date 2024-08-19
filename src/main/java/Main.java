5bd817f01af8e5fb4bba89f78f6aedd1eb405688
ri-gusev <gusev.roman1@mail.ru>
2024-08-19 19:13:20 +0300

    Refactor my HashSet collection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,34 @@
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> nums = new TreeSet<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return -o1.compareTo(o2);
-            }
-        });
+//        Set<Integer> numbs = new TreeSet<>(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return -o1.compareTo(o2);
+//            }
+//        });
+//
+//        for (int i = 0; i <= 50; i++) {
+//            numbs.add(i);
+//        }
+//
+//        for (Integer num : numbs) {
+//            System.out.println(num);
+//        }
+        Map<Integer,String> map = new HashMap<>();
 
-        for (int i = 0; i <= 50; i++) {
-            nums.add(i);
+        for (int i = 0; i < 10; i++) {
+            map.put(i, "Name"+ i);
         }
 
-        for (Integer num : nums) {
-            System.out.println(num);
+        for (Integer y : map.keySet()){
+            System.out.println(y);
         }
+
+        System.out.println(map);
+
+        System.out.println(map.remove(1));
 
 
     }
